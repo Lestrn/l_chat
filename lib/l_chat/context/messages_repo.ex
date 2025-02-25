6e89d95c3b4d82cc87c3d3d4ee31ee70c1855228
Lestrn <ilestrn@gmail.com>
2025-02-25 15:57:01 +0200

    some progress on message repo

---
+++ lib/l_chat/context/messages_repo.ex
@@ -0,0 +1,30 @@
+defmodule LChat.Context.MessagesRepo do
+  import Ecto.Query, warn: false
+  alias LChat.Repo
+  alias LChat.Schemas.Message
+
+  def get_message(id), do: Repo.get(Message, id)
+
+  def get_message_with_preload(id) do
+    from(message in Message,
+      where: message.id == ^id,
+      preload: [:user]
+    )
+    |> Repo.one()
+  end
+
+  def get_messages(), do: Repo.all(Message)
+
+  def get_messages_with_preload() do
+    from(message in Message,
+      preload: [:user]
+    )
+    |> Repo.all()
+  end
+
+  def update_message(id, attrs) do
+    Repo.get(Message, id)
+    |> Message.changeset(attrs, %{validate_msg_ownership: Map.has_key?(attrs, :user_id)})
+    |> Repo.update()
+  end
+end
--- lib/l_chat/schemas/message.ex
+++ lib/l_chat/schemas/message.ex
@@ -8,12 +8,13 @@ defmodule LChat.Schemas.Message do
     timestamps(type: :utc_datetime)
   end
 
-  def changeset(message, attrs) do
+  def changeset(message, attrs, opts \\ %{}) do
     message
     |> cast(attrs, [:content, :user_id])
     |> foreign_key_constraint(:user_id)
     |> validate_required([:content, :user_id])
     |> validate_user_exists()
+    |> maybe_validate_message_ownership(Map.get(opts, :validate_msg_ownership), attrs)
   end
 
   defp validate_user_exists(changeset) do
@@ -24,5 +25,17 @@ defmodule LChat.Schemas.Message do
     else
       changeset
     end
+  end
+
+  defp maybe_validate_message_ownership(changeset, true, attrs) do
+    if attrs.user_id != get_field(changeset, :user_id) do
+      add_error(changeset, :user_id, "Cant change ownership of user")
+    else
+      changeset
+    end
+  end
+
+  defp maybe_validate_message_ownership(changeset, _, _) do
+    changeset
   end
 end
