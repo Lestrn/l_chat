e4650f34a9d998ca759cd99103a5b7f647f6605d
Lestrn <ilestrn@gmail.com>
2025-02-26 16:20:27 +0200

    some progress on chat

--- lib/l_chat_web/live_l_chat/l_chage_page/l_chat_page.ex
+++ lib/l_chat_web/live_l_chat/l_chage_page/l_chat_page.ex
@@ -2,15 +2,21 @@ defmodule LChatWeb.LChatPage do
   use LChatWeb, :live_view
   alias LChat.Schemas.Message
   alias LChatWeb.LChatComponents.FunctionComponents
+  alias LChat.Context.MessagesRepo
 
   def mount(_params, __session, socket) do
     {:ok,
      socket
-     |> assign(message_form: Message.changeset(%Message{}, %{}) |> to_form())}
+     |> assign(message_form: Message.changeset(%Message{}, %{}) |> to_form())
+     |> assign(messages: MessagesRepo.get_messages_with_preload())}
   end
 
-  def handle_event("save", %{"message" => %{"context" => context}}, socket) do
-    IO.inspect(context)
-    {:noreply, socket}
+  def handle_event("save", %{"message" => %{"content" => content}}, socket) do
+    MessagesRepo.create_message(%{content: content, user_id: socket.assigns.current_user.id})
+
+    {:noreply,
+     socket
+     |> assign(messages: MessagesRepo.get_messages_with_preload())
+     |> assign(message_form: Message.changeset(%Message{}, %{}) |> to_form())}
   end
 end
