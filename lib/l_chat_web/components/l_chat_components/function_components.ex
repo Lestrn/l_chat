9829aea6f3381693940da9d6aebbf10b707eefa5
Lestrn <ilestrn@gmail.com>
2025-03-31 12:26:04 +0300

    refactor added to function components

--- lib/l_chat_web/components/l_chat_components/function_components.ex
+++ lib/l_chat_web/components/l_chat_components/function_components.ex
@@ -4,11 +4,81 @@ defmodule LChatWeb.LChatComponents.FunctionComponents do
   attr :icon_name, :string, required: true
   attr :icon_size, :string, required: true
   attr :svg_path, :string, required: true
+
   def icon(assigns) do
     ~H"""
     <svg class={@icon_size}>
       <use href={"#{@svg_path <> @icon_name}"}></use>
     </svg>
+    """
+  end
+
+  attr :id, :string, required: true
+  attr :class, :string, required: true
+  attr :message_id, :integer, required: true
+
+  def context_menu(assigns) do
+    ~H"""
+    <div id={@id} class={@class}>
+      <button
+        phx-click="open_modal_edit_msg"
+        phx-value-message-id={@message_id}
+        class="block w-full text-left px-4 py-2 hover:bg-gray-100"
+      >
+        ✏️ Edit Message
+      </button>
+      <button
+        phx-click="delete_message"
+        phx-value-message-id={@message_id}
+        class="block w-full text-left px-4 py-2 hover:bg-gray-100 text-red-500"
+      >
+        🗑 Delete Message
+      </button>
+    </div>
+    """
+  end
+
+  attr :presences, :list, required: true
+
+  def users_section(assigns) do
+    ~H"""
+    <div class="mr-20 py-5 px-5 bg-white shadow-lg rounded-lg overflow-y-auto">
+      <h2 class="text-lg font-semibold text-gray-800 mb-4">Users Online</h2>
+      <ul class="flex flex-col gap-4">
+        <.user_profile
+          :for={
+            {_user_id, meta} <-
+              @presences
+              |> Enum.sort(fn {_id1, meta1}, {_id2, meta2} -> meta1.is_typing > meta2.is_typing end)
+          }
+          class="flex items-center gap-4 p-4 border-2 border-gray-300 rounded-lg shadow-md transition duration-300 hover:shadow-xl hover:border-gray-400"
+          user_meta={meta}
+        />
+      </ul>
+    </div>
+    """
+  end
+
+  attr :user_meta, :map, required: true
+  attr :class, :string
+
+  def user_profile(assigns) do
+    ~H"""
+    <li class={@class}>
+      <div class="flex items-center justify-center w-12 h-12 rounded-full bg-gradient-to-r from-blue-500 to-purple-500 text-white text-lg font-bold shadow-md">
+        {String.first(@user_meta.username)}
+      </div>
+      <div>
+        <span class="font-medium text-gray-900">{@user_meta.username}</span>
+        <div :if={@user_meta.is_typing} class="flex space-x-1 mt-1">
+          <span class="w-2.5 h-2.5 bg-blue-500 rounded-full animate-typing shadow-md"></span>
+          <span class="w-2.5 h-2.5 bg-blue-500 rounded-full animate-typing shadow-md [animation-delay:0.2s]">
+          </span>
+          <span class="w-2.5 h-2.5 bg-blue-500 rounded-full animate-typing shadow-md [animation-delay:0.4s]">
+          </span>
+        </div>
+      </div>
+    </li>
     """
   end
 end
